0a8a2e8670eb098b3203bffcdef646db26b2b3aa
Justin Middler <justin@yumait.com.au>
2026-05-25 22:56:38 +1000

    Initial Commit

---
+++ HappiE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappiE
+//
+//  Created by Justin Middler on 25/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappiE/HappiEApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappiEApp.swift
+//  HappiE
+//
+//  Created by Justin Middler on 25/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct HappiEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
